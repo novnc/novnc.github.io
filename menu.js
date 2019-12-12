329d6f56a2f6da353c987f67c6d2b3ea157d9107
Samuel Mannehed <samuel@cendio.se>
2019-12-12 02:22:38 +0100

    Use Javascript to include shared top_bar HTML

--- menu.js
+++ menu.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(e) {
+    includeHTML();
+
     let logo_elem = document.getElementById("logo");
     let menu_icon_elem = document.getElementById("menu_icon");
     let menu_elem = document.getElementById("menu");
@@ -17,3 +19,45 @@ document.addEventListener("DOMContentLoaded", function(e) {
         menu_elem.classList.toggle("open");
     }, false);
 });
+
+function includeHTML() {
+    // Loop through all HTML elements
+    let elements = document.getElementsByTagName("*");
+
+    for (let i = 0; i < elements.length; i++) {
+        let elem = elements[i];
+
+        // search for elements with a the right attribute
+        let include_file = elem.getAttribute("include-html");
+        if (include_file) {
+
+            // Make a HTTP request using the attribute value as the file name
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    // Since this isn't synchronous, double-check
+                    // if the attribute is still there
+                    if (this.status == 200 &&
+                        elem.getAttribute("include-html") !== null) {
+
+                        elem.innerHTML = this.responseText + elem.innerHTML;
+                    }
+
+                    if (this.status == 404) {
+                        elem.innerHTML = "Page not found.";
+                    }
+
+                    /* Remove the attribute, and call this function once more: */
+                    elem.removeAttribute("include-html");
+                    includeHTML();
+                }
+            }
+
+            xhttp.open("GET", include_file, true);
+            xhttp.send();
+
+            /* Exit the function: */
+            return;
+        }
+    }
+}
