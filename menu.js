2a66416db88b45362df5b078e813f404e4228e8b
Samuel Mannehed <samuel@cendio.se>
2024-07-18 11:28:58 +0200

    Remove unnecessary shrinking dynamics for top menu
    When scrolling, the top menu and logo shrank a bit. This didn't help
    much with space for the page and was just confusing. Lets remove it.

--- menu.js
+++ menu.js
@@ -66,20 +66,8 @@ function includeHTML() {
 // NORMAL FUNCTIONS
 
 function setupMenuHandlers() {
-    let logo_elem = document.getElementById("logo");
     let menu_icon_elem = document.getElementById("menu_icon");
     let menu_elem = document.getElementById("menu");
-
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 20) {
-            logo_elem.classList.add("logo_small");
-            menu_elem.classList.add("open_small");
-        } else {
-            logo_elem.classList.remove("logo_small");
-            menu_elem.classList.remove("open_small");
-        }
-    }, false);
-
     menu_icon_elem.addEventListener("click", function () {
         menu_elem.classList.toggle("open");
     }, false);
